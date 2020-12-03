3e416b1a8985f81b1d5da381ab1183ad4f6b1d2f
NotSoOld <1000lop@gmail.com>
2020-12-03 19:47:40 +0300

    Seems like function extracted from the stream works faster (or maybe not :D)

--- 2019/day4.cpp
+++ 2019/day4.cpp
@@ -7,46 +7,50 @@
 
 using namespace std;
 
+bool checkPasswordPart1(int password) {
+    string passwordStr = to_string(password);
+    bool hasDouble = false;
+    char lastDigit = '\0';
+    for (auto digit: passwordStr) {
+        if (digit == lastDigit) {
+            hasDouble = true;
+        }
+        if (digit < lastDigit) {
+            return false;
+        }
+        lastDigit = digit;
+    }
+    return hasDouble;
+}
+
+bool checkPasswordPart2(int password) {
+    string passwordStr = to_string(password);
+    auto adjacentDigits = unordered_map<int, int>();
+    char lastDigit = '\0';
+    for (auto digit: passwordStr) {
+        adjacentDigits[digit]++;
+        if (digit < lastDigit) {
+            return false;
+        }
+        lastDigit = digit;
+    }
+    for (auto keyValue: adjacentDigits) {
+        if (keyValue.second == 2) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int countPasswords(int lower, int upper) {
     return stream::MakeStream::closed_range(lower, upper)
-        | stream::op::filter([](int password) {
-            string passwordStr = to_string(password);
-            bool hasDouble = false;
-            char lastDigit = '\0';
-            for (auto digit: passwordStr) {
-                if (digit == lastDigit) {
-                    hasDouble = true;
-                }
-                if (digit < lastDigit) {
-                    return false;
-                }
-                lastDigit = digit;
-            }
-            return hasDouble;
-        })
+        | stream::op::filter(checkPasswordPart1)
         | stream::op::count();
 }
 
 int countPasswordsPart2(int lower, int upper) {
     return stream::MakeStream::closed_range(lower, upper)
-        | stream::op::filter([](int password) {
-            string passwordStr = to_string(password);
-            auto adjacentDigits = unordered_map<int, int>();
-            char lastDigit = '\0';
-            for (auto digit: passwordStr) {
-                adjacentDigits[digit]++;
-                if (digit < lastDigit) {
-                    return false;
-                }
-                lastDigit = digit;
-            }
-            for (auto keyValue: adjacentDigits) {
-                if (keyValue.second == 2) {
-                    return true;
-                }
-            }
-            return false;
-        })
+        | stream::op::filter(checkPasswordPart2)
         | stream::op::count();
 }
 
@@ -54,8 +58,6 @@ void runDay4() {
     int lower = 146810;
     int upper = 612564;
 
-    int count = countPasswords(lower, upper);
-    cout << "Day 4: countPasswords = " << count;
-    count = countPasswordsPart2(lower, upper);
-    cout << ", countPasswordsPart2 = " << count;
+    cout << "Day 4: countPasswords = " << countPasswords(lower, upper);
+    cout << ", countPasswordsPart2 = " << countPasswordsPart2(lower, upper) << endl;
 }
