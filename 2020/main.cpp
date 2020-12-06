0c49170f3313cf8d7a2e85405fbe95ceea5784f7
NotSoOld <1000lop@gmail.com>
2020-12-06 17:03:58 +0300

    Solution for year 2020 Day 4 puzzle (read puzzle description at the official website); added a function to get puzzle input file name to use it for future solutions

--- 2020/main.cpp
+++ 2020/main.cpp
@@ -1,9 +1,18 @@
 #include "main.h"
 
+std::string getInputFile(const std::string& cppFileFullPath) {
+    int fileNamePos = cppFileFullPath.find_last_of('\\');
+    std::string folderPath = cppFileFullPath.substr(0, fileNamePos);
+    std::string fileName = cppFileFullPath.substr(fileNamePos);
+    fileName = fileName.replace(fileName.find("cpp"), sizeof("cpp") - 1, "txt");
+    return folderPath + "\\inputs\\" + fileName;
+}
+
 int main() {
     runDay1();
     runDay2();
     runDay3();
+    runDay4();
     return 0;
 }
 
--- 2020/main.h
+++ 2020/main.h
@@ -1,8 +1,12 @@
 #ifndef ADVENTOFCODE_MAIN_H
 #define ADVENTOFCODE_MAIN_H
 
+#include <string>
+
+std::string getInputFile(const std::string&);
 void runDay1();
 void runDay2();
 void runDay3();
+void runDay4();
 
 #endif //ADVENTOFCODE_MAIN_H
